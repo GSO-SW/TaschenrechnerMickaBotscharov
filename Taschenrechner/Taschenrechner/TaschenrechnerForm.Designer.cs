b69f765ea62083305eda37c80ef6d37fc76aa58c
Epicwin3000 <larsmicka@gmail.com>
2017-03-08 13:58:52 +0100

    Gui erstellt

--- Taschenrechner/Taschenrechner/TaschenrechnerForm.Designer.cs
+++ Taschenrechner/Taschenrechner/TaschenrechnerForm.Designer.cs
@@ -28,12 +28,291 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ausgabeTextBox = new System.Windows.Forms.TextBox();
+            this.eingabeTextBox = new System.Windows.Forms.TextBox();
+            this.einsButton = new System.Windows.Forms.Button();
+            this.zweiButton = new System.Windows.Forms.Button();
+            this.dreiButton = new System.Windows.Forms.Button();
+            this.vierButton = new System.Windows.Forms.Button();
+            this.fünfButton = new System.Windows.Forms.Button();
+            this.sechsButton = new System.Windows.Forms.Button();
+            this.siebenButton = new System.Windows.Forms.Button();
+            this.achtButton = new System.Windows.Forms.Button();
+            this.neunButton = new System.Windows.Forms.Button();
+            this.nullButton = new System.Windows.Forms.Button();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.delButton = new System.Windows.Forms.Button();
+            this.kommerButton = new System.Windows.Forms.Button();
+            this.ausgabeLabel = new System.Windows.Forms.Label();
+            this.eingabeLabel = new System.Windows.Forms.Label();
+            this.MalButton = new System.Windows.Forms.Button();
+            this.geteiltButton = new System.Windows.Forms.Button();
+            this.plusButton = new System.Windows.Forms.Button();
+            this.minusButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ausgabeTextBox
+            // 
+            this.ausgabeTextBox.Location = new System.Drawing.Point(13, 13);
+            this.ausgabeTextBox.Name = "ausgabeTextBox";
+            this.ausgabeTextBox.Size = new System.Drawing.Size(264, 20);
+            this.ausgabeTextBox.TabIndex = 0;
+            // 
+            // eingabeTextBox
+            // 
+            this.eingabeTextBox.Location = new System.Drawing.Point(13, 40);
+            this.eingabeTextBox.Name = "eingabeTextBox";
+            this.eingabeTextBox.Size = new System.Drawing.Size(264, 20);
+            this.eingabeTextBox.TabIndex = 1;
+            // 
+            // einsButton
+            // 
+            this.einsButton.BackColor = System.Drawing.Color.White;
+            this.einsButton.Location = new System.Drawing.Point(14, 229);
+            this.einsButton.Name = "einsButton";
+            this.einsButton.Size = new System.Drawing.Size(75, 23);
+            this.einsButton.TabIndex = 2;
+            this.einsButton.Text = "1";
+            this.einsButton.UseVisualStyleBackColor = false;
+            // 
+            // zweiButton
+            // 
+            this.zweiButton.BackColor = System.Drawing.Color.White;
+            this.zweiButton.Location = new System.Drawing.Point(95, 229);
+            this.zweiButton.Name = "zweiButton";
+            this.zweiButton.Size = new System.Drawing.Size(75, 23);
+            this.zweiButton.TabIndex = 3;
+            this.zweiButton.Text = "2";
+            this.zweiButton.UseVisualStyleBackColor = false;
+            // 
+            // dreiButton
+            // 
+            this.dreiButton.BackColor = System.Drawing.Color.White;
+            this.dreiButton.Location = new System.Drawing.Point(176, 229);
+            this.dreiButton.Name = "dreiButton";
+            this.dreiButton.Size = new System.Drawing.Size(75, 23);
+            this.dreiButton.TabIndex = 4;
+            this.dreiButton.Text = "3";
+            this.dreiButton.UseVisualStyleBackColor = false;
+            // 
+            // vierButton
+            // 
+            this.vierButton.BackColor = System.Drawing.Color.White;
+            this.vierButton.Location = new System.Drawing.Point(14, 200);
+            this.vierButton.Name = "vierButton";
+            this.vierButton.Size = new System.Drawing.Size(75, 23);
+            this.vierButton.TabIndex = 5;
+            this.vierButton.Text = "4";
+            this.vierButton.UseVisualStyleBackColor = false;
+            // 
+            // fünfButton
+            // 
+            this.fünfButton.BackColor = System.Drawing.Color.White;
+            this.fünfButton.Location = new System.Drawing.Point(95, 200);
+            this.fünfButton.Name = "fünfButton";
+            this.fünfButton.Size = new System.Drawing.Size(75, 23);
+            this.fünfButton.TabIndex = 6;
+            this.fünfButton.Text = "5";
+            this.fünfButton.UseVisualStyleBackColor = false;
+            // 
+            // sechsButton
+            // 
+            this.sechsButton.BackColor = System.Drawing.Color.White;
+            this.sechsButton.Location = new System.Drawing.Point(176, 200);
+            this.sechsButton.Name = "sechsButton";
+            this.sechsButton.Size = new System.Drawing.Size(75, 23);
+            this.sechsButton.TabIndex = 7;
+            this.sechsButton.Text = "6";
+            this.sechsButton.UseVisualStyleBackColor = false;
+            // 
+            // siebenButton
+            // 
+            this.siebenButton.BackColor = System.Drawing.Color.White;
+            this.siebenButton.Location = new System.Drawing.Point(15, 171);
+            this.siebenButton.Name = "siebenButton";
+            this.siebenButton.Size = new System.Drawing.Size(75, 23);
+            this.siebenButton.TabIndex = 8;
+            this.siebenButton.Text = "7";
+            this.siebenButton.UseVisualStyleBackColor = false;
+            // 
+            // achtButton
+            // 
+            this.achtButton.BackColor = System.Drawing.Color.White;
+            this.achtButton.Location = new System.Drawing.Point(97, 171);
+            this.achtButton.Name = "achtButton";
+            this.achtButton.Size = new System.Drawing.Size(75, 23);
+            this.achtButton.TabIndex = 9;
+            this.achtButton.Text = "8";
+            this.achtButton.UseVisualStyleBackColor = false;
+            // 
+            // neunButton
+            // 
+            this.neunButton.BackColor = System.Drawing.Color.White;
+            this.neunButton.Location = new System.Drawing.Point(176, 171);
+            this.neunButton.Name = "neunButton";
+            this.neunButton.Size = new System.Drawing.Size(75, 23);
+            this.neunButton.TabIndex = 10;
+            this.neunButton.Text = "9";
+            this.neunButton.UseVisualStyleBackColor = false;
+            // 
+            // nullButton
+            // 
+            this.nullButton.BackColor = System.Drawing.Color.White;
+            this.nullButton.Location = new System.Drawing.Point(15, 258);
+            this.nullButton.Name = "nullButton";
+            this.nullButton.Size = new System.Drawing.Size(157, 23);
+            this.nullButton.TabIndex = 11;
+            this.nullButton.Text = "0";
+            this.nullButton.UseVisualStyleBackColor = false;
+            // 
+            // enterButton
+            // 
+            this.enterButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.enterButton.Location = new System.Drawing.Point(295, 316);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 12;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = false;
+            // 
+            // delButton
+            // 
+            this.delButton.BackColor = System.Drawing.Color.OrangeRed;
+            this.delButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.delButton.Location = new System.Drawing.Point(176, 126);
+            this.delButton.Name = "delButton";
+            this.delButton.Size = new System.Drawing.Size(75, 23);
+            this.delButton.TabIndex = 13;
+            this.delButton.Text = "DEL";
+            this.delButton.UseVisualStyleBackColor = false;
+            // 
+            // kommerButton
+            // 
+            this.kommerButton.BackColor = System.Drawing.Color.White;
+            this.kommerButton.Location = new System.Drawing.Point(176, 257);
+            this.kommerButton.Name = "kommerButton";
+            this.kommerButton.Size = new System.Drawing.Size(75, 23);
+            this.kommerButton.TabIndex = 14;
+            this.kommerButton.Text = ",";
+            this.kommerButton.UseVisualStyleBackColor = false;
+            // 
+            // ausgabeLabel
+            // 
+            this.ausgabeLabel.AutoSize = true;
+            this.ausgabeLabel.BackColor = System.Drawing.Color.Fuchsia;
+            this.ausgabeLabel.Location = new System.Drawing.Point(284, 19);
+            this.ausgabeLabel.Name = "ausgabeLabel";
+            this.ausgabeLabel.Size = new System.Drawing.Size(49, 13);
+            this.ausgabeLabel.TabIndex = 15;
+            this.ausgabeLabel.Text = "Ausgabe";
+            // 
+            // eingabeLabel
+            // 
+            this.eingabeLabel.AutoSize = true;
+            this.eingabeLabel.BackColor = System.Drawing.Color.Fuchsia;
+            this.eingabeLabel.Location = new System.Drawing.Point(284, 40);
+            this.eingabeLabel.Name = "eingabeLabel";
+            this.eingabeLabel.Size = new System.Drawing.Size(46, 13);
+            this.eingabeLabel.TabIndex = 16;
+            this.eingabeLabel.Text = "Eingabe";
+            // 
+            // MalButton
+            // 
+            this.MalButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.MalButton.Location = new System.Drawing.Point(295, 228);
+            this.MalButton.Name = "MalButton";
+            this.MalButton.Size = new System.Drawing.Size(75, 23);
+            this.MalButton.TabIndex = 17;
+            this.MalButton.Text = "x";
+            this.MalButton.UseVisualStyleBackColor = false;
+            // 
+            // geteiltButton
+            // 
+            this.geteiltButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.geteiltButton.Location = new System.Drawing.Point(295, 258);
+            this.geteiltButton.Name = "geteiltButton";
+            this.geteiltButton.Size = new System.Drawing.Size(75, 23);
+            this.geteiltButton.TabIndex = 18;
+            this.geteiltButton.Text = ":";
+            this.geteiltButton.UseVisualStyleBackColor = false;
+            // 
+            // plusButton
+            // 
+            this.plusButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.plusButton.Location = new System.Drawing.Point(295, 170);
+            this.plusButton.Name = "plusButton";
+            this.plusButton.Size = new System.Drawing.Size(75, 23);
+            this.plusButton.TabIndex = 19;
+            this.plusButton.Text = "+";
+            this.plusButton.UseVisualStyleBackColor = false;
+            // 
+            // minusButton
+            // 
+            this.minusButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.minusButton.Location = new System.Drawing.Point(295, 200);
+            this.minusButton.Name = "minusButton";
+            this.minusButton.Size = new System.Drawing.Size(75, 23);
+            this.minusButton.TabIndex = 20;
+            this.minusButton.Text = "-";
+            this.minusButton.UseVisualStyleBackColor = false;
+            // 
+            // TaschenrechnerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(404, 370);
+            this.Controls.Add(this.minusButton);
+            this.Controls.Add(this.plusButton);
+            this.Controls.Add(this.geteiltButton);
+            this.Controls.Add(this.MalButton);
+            this.Controls.Add(this.eingabeLabel);
+            this.Controls.Add(this.ausgabeLabel);
+            this.Controls.Add(this.kommerButton);
+            this.Controls.Add(this.delButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.nullButton);
+            this.Controls.Add(this.neunButton);
+            this.Controls.Add(this.achtButton);
+            this.Controls.Add(this.siebenButton);
+            this.Controls.Add(this.sechsButton);
+            this.Controls.Add(this.fünfButton);
+            this.Controls.Add(this.vierButton);
+            this.Controls.Add(this.dreiButton);
+            this.Controls.Add(this.zweiButton);
+            this.Controls.Add(this.einsButton);
+            this.Controls.Add(this.eingabeTextBox);
+            this.Controls.Add(this.ausgabeTextBox);
+            this.Name = "TaschenrechnerForm";
+            this.Text = "Taschenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ausgabeTextBox;
+        private System.Windows.Forms.TextBox eingabeTextBox;
+        private System.Windows.Forms.Button einsButton;
+        private System.Windows.Forms.Button zweiButton;
+        private System.Windows.Forms.Button dreiButton;
+        private System.Windows.Forms.Button vierButton;
+        private System.Windows.Forms.Button fünfButton;
+        private System.Windows.Forms.Button sechsButton;
+        private System.Windows.Forms.Button siebenButton;
+        private System.Windows.Forms.Button achtButton;
+        private System.Windows.Forms.Button neunButton;
+        private System.Windows.Forms.Button nullButton;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button delButton;
+        private System.Windows.Forms.Button kommerButton;
+        private System.Windows.Forms.Label ausgabeLabel;
+        private System.Windows.Forms.Label eingabeLabel;
+        private System.Windows.Forms.Button MalButton;
+        private System.Windows.Forms.Button geteiltButton;
+        private System.Windows.Forms.Button plusButton;
+        private System.Windows.Forms.Button minusButton;
     }
 }
 
